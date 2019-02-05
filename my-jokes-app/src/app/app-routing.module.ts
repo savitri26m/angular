350b8278b42841cb864aab3e52e00b2c51c9c4a3
bhavesh <bhavesh.vyas@neosofttech.com>
2019-02-05 18:23:10 +0530

    added register template and validation

---
+++ my-jokes-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'auth/register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
